667224ef90987f659ae0f596288d3e80096ff202
Anxietie <84636109+Anxietie@users.noreply.github.com>
2021-06-01 12:32:05 -0400

    Update Dice.java

--- src/com.me/Dice.java
+++ src/com.me/Dice.java
@@ -1,13 +1,11 @@
 package com.me;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class Dice {
-	public short getNextRoll() {
-		Scanner scanner = new Scanner(System.in);
+	public int getNextRoll() {
 		Random rand = new Random();
-		short roll = (short)(rand.nextInt(6)+1);
+		int roll = rand.nextInt(6)+1;
 		return(roll);
 	}
 }

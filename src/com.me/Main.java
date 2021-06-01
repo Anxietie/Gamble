bf68f155522d40370aea106972a6f6a8ee416ff4
Anxietie <84636109+Anxietie@users.noreply.github.com>
2021-06-01 12:32:24 -0400

    Update Main.java

--- src/com.me/Main.java
+++ src/com.me/Main.java
@@ -4,42 +4,57 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-	static User user = new User();
-	static double money = user.getMoney();
-	static NumberFormat usd = NumberFormat.getCurrencyInstance();
+
 	public static void main(String[] args) {
-		Bet bid = new Bet();
-		Dice dice = new Dice();
-		Scanner scanner = new Scanner(System.in);
-		short guess = user.getGuess();
-		double bidding = bid.getBid();
-		short landing = dice.getNextRoll();
-		String again;
-		if (guess==landing) {
-			System.out.println("Congratulations!\nYou won "+usd.format(bidding*3)+"!");
-			money+=bidding*3.0;
-			user.saveMoney();
-			System.out.println("You now have: "+usd.format(money));
-		}
-		else {
-			System.out.println("You lost.");
-			money-=bidding;
-			user.saveMoney();
-			System.out.println("You now have: "+usd.format(money));
-		}
+		boolean again = false;
 		while (true) {
-			System.out.print("Would you like to play again? Yes or no? ");
-			again = scanner.next().toLowerCase().trim();
-			if (again.equals("y") || again.equals("yes"))
-				main(args);
-			else if (again.equals("n") || again.equals("no")) {
-				System.out.println("Thank you for playing!");
-				scanner.close();
-				System.exit(0);
-			}
+			// Initializes some necessary variables
+			NumberFormat usd = NumberFormat.getCurrencyInstance();
+			Scanner scanner = new Scanner(System.in);
+			boolean correct = false;
+			// If they say yes to play again it prints "Ready?" instead of "Welcome!" every time to make it look more appealing
+			if (!again)
+				System.out.println("Welcome!");
 			else
-				System.out.println("Please type yes or no!");
+				System.out.println("Ready?");
+			// Dice and user objects
+			Dice die = new Dice();
+			User user = new User();
+			// Creates the user file
+			user.createFile();
+			if (user.getGuess()==die.getNextRoll())
+				correct=true;
+			if (user.getMoney()<=0) {
+				System.out.println("Lmao you're broke. Here's $50");
+				user.money=50.0;
+			}
+			double bet = user.getBet();
+			// If they won or lost
+			if (correct) {
+				System.out.println("Congratulations! You won: "+usd.format(bet*6));
+				user.money+=bet*6;
+				user.saveMoney();
+			}
+			else {
+				user.money-=bet;
+				user.saveMoney();
+				System.out.println("You lost. You now have: "+usd.format(user.money));
+			}
+			// Play again?
+			System.out.print("Would you like to play again? y/n ");
+			while (true) {
+				String pAgain = scanner.next();
+				if (pAgain.equalsIgnoreCase("yes") || pAgain.equalsIgnoreCase("y")) {
+					again = true;
+					break;
+				}
+				else if (pAgain.equalsIgnoreCase("no") || pAgain.equalsIgnoreCase("n")) {
+					System.out.println("Byebye!");
+					scanner.close();
+					System.exit(0);
+				}
+				System.out.print("Please type y or n! (Yes and No also work). ");
+			}
 		}
 	}
-
 }
